3f7b210b8942510ec493a2d30bee8a94a8d45351
Faramarz <fdorani@cisco.com>
2020-12-11 14:58:45 -0500

    adding dynamic settings through mgmt service

--- etc/mgmt_settings.py
+++ etc/mgmt_settings.py
@@ -117,12 +117,62 @@ MICROSERVICES_LOGS_ROOT = '/s3/logs'
 
 # Logs
 # ----
-# this service logs to management directory, unlike all other services which
-# assumes that /s3/logs/ dir is their logging directory
-LOGGING_ROOT = os.environ.get('SERVICE_LOGS_DIR', '/s3/logs/management')
-
+LOGGING_ROOT = os.environ.get('SERVICE_LOGS_DIR', '/tmp')
 LOGGING = {
-    
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format':'%(asctime)s [%(levelname)s]: %(message)s',
+            'datefmt':'%d/%m/%Y %H:%M:%S'
+        },
+        'verbose': {
+            'format':'%(asctime)s [%(levelname)s] %(name)s.%(funcName)s(): %(message)s',
+            'datefmt':'%d/%m/%Y %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'console': {
+            'formatter': 'verbose',
+            'class': 'logging.StreamHandler',
+        },
+        'ServiceFileHandler': {
+            'formatter':'verbose',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOGGING_ROOT, 's3.log'),
+            'maxBytes': 1024*1024*1024,
+            'backupCount': 5,
+        },
+        'ErrorFileHandler': {
+            'formatter':'verbose',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOGGING_ROOT, 'error.log'),
+            'maxBytes': 1024*1024*1024,
+            'backupCount': 5,
+        },
+    },
+    'loggers': {
+        'gunicorn.access': {
+            'handlers': ['ServiceFileHandler', 'console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'gunicorn.error': {
+            'handlers': ['ErrorFileHandler'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'aiohttp.server': {
+            'handlers': ['ErrorFileHandler'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        '': {
+            'handlers': ['ServiceFileHandler', 'console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
 }
 
 # Cisco OAuth Registry - COAR
