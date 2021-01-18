00c4b9233f1b22f4a705c16d9058129690794e2b
Andrew Delph <adelph@cisco.com>
2021-01-18 22:24:30 +0000

    basics to setup script complete

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
-echo "ADVERTISED_URL: $1";
+RED='\033[0;31m'
+YLW='\033[0;33m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
+echo -e "ADVERTISED_URL: ${BLUE}$1${NC}";
 [ -z "$1" ] && echo "ADVERTISED_URL not set. Enter the host url you would like to advertise." && exit;
 
 while true; do
@@ -10,6 +17,12 @@ while true; do
     esac
 done
 
+sed -i '/ADVERTISED_URL/d' ./.env
+echo 'ADVERTISED_URL=$1' >> ./.env
+
+
+echo 'Creating data and logs directory';
+
 mkdir logs
 mkdir logs/database
 chmod 777 -R  logs
@@ -19,9 +32,23 @@ mkdir data/elastic_snapshots
 mkdir data/mysql
 chmod 777 -R  data
 
-sed -i '/ADVERTISED_URL/d' ./.env
-echo 'ADVERTISED_URL=$1' >> ./.env
+echo -e "${YLW}Warning:${NC}";
+echo -e "${YLW}Elasticsearch uses a mmapfs directory by default to store its indices. The default operating system limits on mmap counts is likely to be too low, which may result in out of memory exceptions.${NC}";
+echo -e "${RED}Writing 'sysctl -w vm.max_map_count=262144' to '/etc/sysctl.conf'${NC}";
+
+while true; do
+    read -p "Do you wish to continue?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 sed -i '/max_map_count/d' /etc/sysctl.conf
-echo 'sysctl -w vm.max_map_count=262141' >> /etc/sysctl.conf
+echo 'sysctl -w vm.max_map_count=262144' >> /etc/sysctl.conf
+
+echo -e "${GREEN}Setup complete!${NC}";
+
+echo -e "${GREEN}run: 'docker-compose up -d' to start xpresso.${NC}";
 
